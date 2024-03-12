9b1fd46c6bc3e72c857a04aed536f511f4d335e5
techmoves <muhndlovu28@gmail.com>
2024-03-12 13:04:47 +0200

    auth pages and add icon ans assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./_auth/AuthLayout";
-import RootLayout from "./_roots/RootLayout";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import { Home } from "@/_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+
+import "./globals.css";
+
+const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
         {/* private routes */}
-        <Route element={<AuthLayout />} />
-        <Route path="/sigh-in" element={<SigninForm />} />
-        <Route path="/sign-Up" element={<signUpForm />} />
-        <Route />
-        {/* private routes */}
-        <Route element={<RootLayout />} />
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+// import { AuthProvider } from "@/context/AuthContext";
+
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
